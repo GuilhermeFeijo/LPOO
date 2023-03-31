d2bb612d4eaf9f5df6705dea93b61f663952ad07
GuilhermeFeijo <guilhermezfeijo99@gmail.com>
2023-03-31 16:46:44 -0300

    feat: criada interface automovel

---
+++ objetivo2_lista1_herenca_multipla/src/model/Automovel.java
@@ -0,0 +1,10 @@
+package model;
+
+public interface Automovel {
+    String getRenavan();
+    void setRenavan(String renavan);
+    String getChassi();
+    void setChassi(String chassi);
+    String getPlaca();
+    void setPlaca(String placa);
+}
